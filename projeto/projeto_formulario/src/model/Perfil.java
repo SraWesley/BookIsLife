0726dd4768cb36d7243d483f281601d2a7771610
unknown <lauratellesgomes@hotmail.com>
2015-09-15 20:20:53 -0300

    (Uma pequena parte do formulário)

---
+++ projeto/projeto_formulario/src/model/Perfil.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Perfil {
+
+	public String local;
+	public String idade;
+	public String nascimento;
+	public String escola;
+	
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getIdade() {
+		return idade;
+	}
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+	public String getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
+	public String getEscola() {
+		return escola;
+	}
+	public void setEscola(String escola) {
+		this.escola = escola;
+	}
+}
