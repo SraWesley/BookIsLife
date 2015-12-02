50bcb3973d88c7216892cb415b0043c789f68b95
BeahIF <BeatrizDaCosta1@gmail.com>
2015-12-02 15:08:29 -0200

    Tratando erros de entrada

--- PROJETOUNICO/src/controlador/SalvaControlador.java
+++ PROJETOUNICO/src/controlador/SalvaControlador.java
@@ -2,6 +2,9 @@ package controlador;
 
 import java.io.*;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Scanner;
+
 import modelo.Livro;
 import persistencia.LivroDAO;
 import spark.*;
@@ -17,31 +20,40 @@ public class SalvaControlador implements TemplateViewRoute {
 		livro.setNome(req.queryMap("nome").value());
 		livro.setEscritor(req.queryMap("escritor").value());
 		livro.setEditora(req.queryMap("editora").value());
-
-		/// ver se tem necessidade disso
+		String erro2="";
 		if (livro.getNome().length() < 3) {
 			String erro = "";
-
-			try {
-				erro = URLEncoder.encode("Título deve ter pelo menos 3 caracteres", "UTF-8");
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
+			erro = "Título deve ter pelo menos 3 caracteres";
+			HashMap mapa = new HashMap();
+			mapa.put("erro", erro);
+			return new ModelAndView(mapa,"livro_cadastrar.html");
+		}else {
+			File file = new File("livros/" + "nomes.csv");
+			String nome = livro.getNome();
+			System.out.println("Estou no else");
+			if (file.exists()) {
+				try {System.out.println(nome);
+					Scanner scan = new Scanner(file);
+					String linha = scan.nextLine();
+					String[] colunas = linha.split(";");
+					scan.close();
+					for(int i = 0; i < colunas.length; i++){
+						if(nome.equals(colunas[i])){
+							System.out.println(colunas[i]);
+							System.out.println("é igual");
+							erro2 = "Livro já existente!";
+						}
+					}
+				} catch (FileNotFoundException e) {
+				}
+			HashMap mapa1 = new HashMap();
+			mapa1.put("erro2", erro2);
+			return new ModelAndView(mapa1,"livro_cadastrar.html");
 			}
-
-			resp.redirect("/novo?erro=" + erro);
 		}
-
-		else {
 			dao.save(livro);
 			resp.redirect("/lista");
+			return null;
 		}
-		// if (filme.getTitulo().length() < 3) { // invÃ¡lido
-		// Spark.halt(400, "TÃ­tulo deve ter pelo menos 3 caracteres");
-		// } else { // vÃ¡lido
-		// dao.save(filme);
-		// resp.redirect("/lista");
-		// }
-
-		return null;
-	}
+		
 }
